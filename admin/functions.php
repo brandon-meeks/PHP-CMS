232274258f25fcee043af1ece35c7bfa298fc0b6
Brandon Meeks <brandon@desine.co>
2016-08-03 16:15:32 -0400

    updated user password encryption

--- admin/functions.php
+++ admin/functions.php
@@ -549,7 +549,44 @@ function deleteUser() {
     
 }
 
+function usersOnline() {
 
+    if(isset($_GET['onlineusers'])) {
+
+
+        global $connection;
+
+        if(!$connection) {
+
+            session_start();
+
+            include("../includes/db.php");
+
+            $session = session_id();
+            $time = time();
+            $time_out_in_seconds = 05;
+            $time_out = $time - $time_out_in_seconds;
+
+            $query = "SELECT * FROM users_online WHERE session = '$session' ";
+            $send_query = mysqli_query($connection, $query);
+            $count = mysqli_num_rows($send_query);
+
+            if($count == NULL) {
+                mysqli_query($connection, "INSERT INTO users_online(session, session_time) VALUES('$session', '$time') ");
+            } else {
+                mysqli_query($connection, "UPDATE users_online SET session_time = '$time' WHERE session = '$session' ");
+            }
+
+            $users_online_query = "SELECT * FROM users_online WHERE session_time > '$time_out' ";
+            $send_users_query = mysqli_query($connection, $users_online_query);
+            echo $count_users = mysqli_num_rows($send_users_query);
+        }
+
+    }
+
+}
+
+usersOnline();
 
 
 ?>
